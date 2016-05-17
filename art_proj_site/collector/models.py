f30e7bc73c119cb031c462871c425da0fd245eca
Jeremy Low <jeremylow@gmail.com>
2016-05-17 18:17:16 -0400

    add repr functions for each model

--- art_proj_site/collector/models.py
+++ art_proj_site/collector/models.py
@@ -7,11 +7,20 @@ class TwitterUser(models.Model):
     face_hash = models.TextField(blank=True)
     image = models.ImageField(null=True)
 
+    def __str__(self):
+        return "TwitterUser(name={0!r})".format(self.username)
+
 
 class Location(models.Model):
     loc_name = models.CharField(max_length=255)
     latitude = models.FloatField(default=0.0)
     longitude = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return "Location(name={0!r}, latitude={1}, longitude={2})".format(
+            self.loc_name,
+            self.latitude,
+            self.longitude)
 
 
 class Tweet(models.Model):
@@ -19,3 +28,10 @@ class Tweet(models.Model):
     tweet_text = models.TextField()
     tweet_date = models.DateField()
     tweet_loc = models.OneToOneField(Location, null=True)
+
+    def __str__(self):
+        return "Tweet(tweet_user={0!r}, tweet_text={1!r}, tweet_date={2}, tweet_loc={3!r})".format(
+            self.tweet_user.username,
+            self.tweet_text,
+            self.tweet_date,
+            self.tweet_loc.loc_name)
