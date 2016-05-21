5c4e2d633d3b4a4ed8de74c9741b855ae85a0246
Jeremy Low <jeremylow@gmail.com>
2016-05-21 14:10:22 -0400

    add licenses

--- cyberpunkd/collector/scraper.py
+++ cyberpunkd/collector/scraper.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import division, print_function
 
 import curio
@@ -28,6 +41,8 @@ from collector.models import (  # NOQA
     TwitterUser,
     Tweet,
     Location)
+
+__author__ = "Jeremy Low <jeremylow@gmail.com>"
 
 
 def post_slack(msg):
